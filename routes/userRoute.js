fc2b8c6ba620011d2d8bdd9f51b31f9d9dd0ef83
hemanthkumar525 <111293727+hemanthkumar525@users.noreply.github.com>
2025-10-19 12:29:12 +0530

    Fix formatting and add missing newline at end of file

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -15,19 +15,20 @@ import {
   updateUserProfile,
 } from "../controllers/userController.js";
 import { isAdminRoute, protectRoute } from "../middleware/authMiddleware.js";
-
+ 
 const router = express.Router();
-
+ 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
-
+ 
 router.get("/get-team", protectRoute, isAdminRoute, getTeamList);
 router.get("/notifications", protectRoute, getNotificationsList);
 router.get("/get-status", protectRoute, isAdminRoute, getUserTaskStatus);
-
+router.get("/profile", protectRoute, getUserProfile);
+ 
 router.put("/profile", protectRoute, updateUserProfile);
 router.put("/read-noti", protectRoute, markNotificationRead);
 router.put("/change-password", protectRoute, changeUserPassword);
@@ -36,5 +37,5 @@ router
   .route("/:id")
   .put(protectRoute, isAdminRoute, activateUserProfile)
   .delete(protectRoute, isAdminRoute, deleteUserProfile);
-
+ 
 export default router;
