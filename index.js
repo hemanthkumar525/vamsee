4e04bb0a38ac5ed1ef2afad37b2da6975046f21e
hemanthkumar525 <111293727+hemanthkumar525@users.noreply.github.com>
2025-11-03 10:39:27 +0530

    Fix typo in URL for task.seopluswpress.com

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const corsOptions = {
     "undefined",
     "https://seashell-caterpillar-640791.hostingersite.com",
     "https://tan-aardvark-644850.hostingersite.com",
-    "https://task.seopluswpress.com/'
+    "https://task.seopluswpress.com'
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
