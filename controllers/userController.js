b4fb13a6c280f3816f90e5263e6c5fdac9ec6436
hemanthkumar525 <111293727+hemanthkumar525@users.noreply.github.com>
2025-12-01 11:46:08 +0530

    Update email transporter configuration to use Gmail service

--- controllers/userController.js
+++ controllers/userController.js
@@ -286,10 +286,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
  
   // Email setup
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-port: 465,
-secure: true,
-
+    service: 'gmail'
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
